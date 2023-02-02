715fd567e7fd4f619e16f8322a90a31f3f2bd156
elcomendante <karol@finsane.com>
2023-02-02 10:51:07 +0000

    code in

--- programs/solana-escrow/src/lib.rs
+++ programs/solana-escrow/src/lib.rs
@@ -6,10 +6,53 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_escrow(ctx: Context<CreateEscrow>, amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+
+        // Set from
+        escrow.from = ctx.accounts.from.key();
+        // Set to
+        escrow.to = ctx.accounts.to.key();
+        // set amount
+        escrow.amount = amount;
+
         Ok(())
     }
 }
 
+/// CreateEscrow context
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateEscrow<'info> {
+    // Escrow Account PDA
+    #[account(
+        init,
+        // State account seed uses the string "state" and the users' key. 
+        // Note that we can only have 1 active transaction
+        seeds = [b"escrow".as_ref(), from.key().as_ref(), to.key().as_ref()],
+        bump,
+        payer = from,
+        space = size_of::<EscrowAccount>() + 16
+    )]
+    pub escrow: Account<'info, EscrowAccount>,
+
+    #[account(mut)]
+    pub from: Signer<'info>,
+    /// CHECK: safe
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// Escrow Account Structure
+#[account]
+pub struct EscrowAccount {
+    // From address
+    pub from: Pubkey,
+
+    // To address
+    pub to: Pubkey,
+
+    // Amount that is owed
+    pub amount: u64,
+}
--- tests/solana-escrow.ts
+++ tests/solana-escrow.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaEscrow } from "../target/types/solana_escrow";
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { BN } from "bn.js";
+import { assert } from "chai";
 
 describe("solana-escrow", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaEscrow as Program<SolanaEscrow>;
+  const program = anchor.workspace.DemoPda as Program<SolanaEscrow>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const publicKey = anchor.AnchorProvider.local().wallet.publicKey;
+    const toWallet: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const [escrowPDA] = await anchor.web3.PublicKey.findProgramAddress([
+        utf8.encode('escrow'),
+        publicKey.toBuffer(), 
+        toWallet.publicKey.toBuffer()
+      ],
+      program.programId
+    );
+    console.log("escrowPDA", escrowPDA);
+    await program.methods.createEscrow(new BN(32)).accounts({
+      from: publicKey,
+      to: toWallet.publicKey,
+      systemProgram:  anchor.web3.SystemProgram.programId,
+      escrow: escrowPDA
+    }).rpc();
+    const escrowAccount = await program.account.escrowAccount.fetch(escrowPDA);
+    console.log(escrowAccount);
+    assert.equal(escrowAccount.amount.toNumber(), 32);
+    assert.isTrue(escrowAccount.from.equals(publicKey));
+    assert.isTrue(escrowAccount.to.equals(toWallet.publicKey));
   });
 });
+
